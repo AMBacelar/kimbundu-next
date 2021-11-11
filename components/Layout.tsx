9e466c7ee4771960e49016fefac73057fdad4049
Adilson Bacelar <a.m.bacelar@gmail.com>
2021-11-11 20:47:36 +0000

    initial routing complete

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -24,6 +24,10 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
           <a>About</a>
         </Link>{' '}
         |{' '}
+        <Link href="/search">
+          <a>Search</a>
+        </Link>{' '}
+        |{' '}
         <Link href="/users">
           <a>Users List</a>
         </Link>{' '}
---
+++ pages/search/index.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import Layout from '../../components/Layout'
+
+const SearchResultPage = () => (
+  <Layout title="Search | Next.js + TypeScript Example">
+    <h1>Search results Next.js 👋</h1>
+    <p>
+      Here is where we will have the search results
+    </p>
+  </Layout>
+)
+
+export default SearchResultPage
